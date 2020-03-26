f12097684eb730ad2a4dfb5b674a051a959863ba
Mark Spannbauer <msmarkieer@gmail.com>
2020-03-26 13:32:11 -0500

    changed a variable name

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -5,7 +5,7 @@ const koalaRouter = express.Router();
 const pool = require('../modules/pool');
 
 // GET
-router.get('/', (req, res) => {
+koalaRouter.get('/', (req, res) => {
     let queryText = 'SELECT * FROM "koalas" ORDER BY "name";';
     pool.query(queryText).then(result => {
         res.send(result.rows);
