2bc3003d00a0ec3eb95e9a2169cc0a2b313829db
Mark Spannbauer <msmarkieer@gmail.com>
2020-03-26 13:24:52 -0500

    added backend for getting koalas

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -5,7 +5,16 @@ const koalaRouter = express.Router();
 const pool = require('../modules/pool');
 
 // GET
-
+router.get('/', (req, res) => {
+    let queryText = 'SELECT * FROM "koalas" ORDER BY "name";';
+    pool.query(queryText).then(result => {
+        res.send(result.rows);
+    })
+        .catch(error => {
+            console.log('error getting books', error);
+            res.sendStatus(500);
+        });
+});
 
 // POST
 
